042d84f889326e66d95d2964b2d3cbff8c6d3b42
Dennis F <romeoraven@gmail.com>
2019-07-22 17:48:27 -0400

    Added -e to some echo lines

--- basic-info.sh
+++ basic-info.sh
@@ -10,9 +10,9 @@ H2="\e[1m \e[97m";		#BOLD and WHITE;
 ## Info
 echo -e "\n$H1===========$H2 Server Info $H1===========$RS";
 echo -e "Hostname: $(hostname)";
-echo "IP: $(hostname -i)\n";
+echo -e "IP: $(hostname -i)\n";
 ## Server time, uptime, users logged in and load avgs
 w;
-if [[ -f /etc/redhat-release ]]; then echo "OS: $(cat /etc/redhat-release)"; fi
-echo "CPUs: $(nproc)";
+if [[ -f /etc/redhat-release ]]; then echo -e "OS: $(cat /etc/redhat-release)"; fi
+echo -e "CPUs: $(nproc)";
 free -h;
