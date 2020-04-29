8ff2b8b379e9cc779d488b65f3a3cdb3f1f79dd9
Dennis F <romeoraven@gmail.com>
2020-04-29 19:12:35 -0400

    Added line count and du check

--- apache-check.sh
+++ apache-check.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+## Version: .02
+## Author: RomeoRaven
+
 if [[ -f /etc/httpd/logs/error_log ]]; then
-	echo -e "There is an error_log file"
+LINE_COUNT=$(cat /etc/httpd/logs/error_log | wc -l);
+LOGFILE_SIZE_APACHE=$(du -sh /etc/httpd/logs/error_log);
+	echo -e "Lines: ${LINE_COUNT}\nFile Size: ${LOGFILE_SIZE_APACHE}\n"
 fi
