e604904981cc98f3f82d58c11be106e486482a1c
Dennis F <romeoraven@gmail.com>
2020-04-29 14:14:48 -0400

    fix - spacingFixed spacing on pipes

--- colors_test.sh
+++ colors_test.sh
@@ -7,7 +7,7 @@ source <(curl -ks https://raw.githubusercontent.com/RomeoRaven/ONEScripts/master
 # default: echo -e "${cG}THING${cR}"
 
 echo -e "Colors   |  ${fB}Color Bold${fR}  |  ${fU}Color Underline${fR}  |   ${fH}Color Blink${fR}   |    ${fS}Color Shade${fR}   |"
-echo -e "Default   | ${fB} Default Bold${fR}   | ${fU}Default Underline${cR}${fR}    | ${fBl} Default Blink${cR}${fR}    | ${fS} Default Shade${cR}${fR} "
+echo -e "Default  | ${fB} Default Bold${fR} | ${fU}Default Underline${cR}${fR}  | ${fBl} Default Blink${cR}${fR}  | ${fS} Default Shade${cR}${fR} "
 echo -e "${cN} Black   | ${cN}${fB} Black Bold${fR}   | ${cN}${fU}Black Underline${cR}${fR}    | ${cN}${fBl} Black Blink${cR}${fR}    | ${cN}${fS} Black Shade${cR}${fR} "
 echo -e "${cG} Green   | ${cG}${fB} Green Bold${fR}   | ${cG}${fU}Green Underline${cR}${fR}    | ${cG}${fBl} Green Blink${cR}${fR}    | ${cG}${fS} Green Shade${cR}${fR} "
 echo -e "${cY} Yellow  | ${cY}${fB} Yellow Bold${fR}  | ${cY}${fU}Yellow Underline${cR}${fR}   | ${cY}${fBl} Yellow Blink${cR}${fR}   | ${cY}${fS} Yellow Shade${cR}${fR} "
