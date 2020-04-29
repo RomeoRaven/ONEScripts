91b31de9174ddcde50f90e01cd8a629f32f2f3cd
Dennis F <romeoraven@gmail.com>
2020-04-29 14:59:03 -0400

    Fix - Spacing issues
    ...

--- colors_test.sh
+++ colors_test.sh
@@ -7,8 +7,8 @@ source <(curl -ks https://raw.githubusercontent.com/RomeoRaven/ONEScripts/master
 # default: echo -e "${cG}THING${cRs}"
 
 echo ""
-echo -e "  Colors   |  ${fB}Color Bold${fR}  |  ${fU}Color Underline${fR}  |   ${fH}Color Blink${fR}   |    ${fS}Color Shade${fR}   |"
-echo -e " Default  | ${fB} Default Bold${fR} | ${fU}Default Underline${fR}  | ${fBl} Default Blink${fR}  | ${fS} Default Shade${fR} "
+echo -e "  Colors  |  ${fB}Color Bold${fR}  |  ${fU}Color Underline${fR}   |   ${fH}Color Blink${fR}   |    ${fS}Color Shade${fR}   |"
+echo -e " Default | ${fB} Default Bold${fR} | ${fU}Default Underline${fR}  | ${fBl} Default Blink${fR}  | ${fS} Default Shade${fR} "
 echo -e "${cN} Black   | ${cN}${fB} Black Bold${fR}   | ${cN}${fU}Black Underline${cRs}${fR}    | ${cN}${fBl} Black Blink${cRs}${fR}    | ${cN}${fS} Black Shade${cRs}${fR} "
 echo -e "${cR} Red     | ${cR}${fB} Red Bold${fR}     | ${cR}${fU}Red Underline${cRs}${fR}      | ${cR}${fBl} Red Blink${cRs}${fR}      | ${cR}${fS} Red Shade${cRs}${fR} "
 echo -e "${cG} Green   | ${cG}${fB} Green Bold${fR}   | ${cG}${fU}Green Underline${cRs}${fR}    | ${cG}${fBl} Green Blink${cRs}${fR}    | ${cG}${fS} Green Shade${cRs}${fR} "
@@ -18,6 +18,7 @@ echo -e "${cM} Magenta | ${cM}${fB} Magenta Bold${fR} | ${cM}${fU}Magenta Underl
 echo -e "${cP} Purple  | ${cP}${fB} Purple Bold${fR}  | ${cP}${fU}Purple Underline${cRs}${fR}   | ${cP}${fBl} Purple Blink${cRs}${fR}   | ${cP}${fS} Purple Shade${cRs}${fR} "
 echo -e "${cC} Cyan    | ${cC}${fB} Cyan Bold${fR}    | ${cC}${fU}Cyan Underline${cRs}${fR}     | ${cC}${fBl} Cyan Blink${cRs}${fR}     | ${cC}${fS} Cyan Shade${cRs}${fR} "
 
+echo ""
 echo -e " Bright = Bold"
 echo -e "${bN} Bright Black ${cRs}"
 echo -e "${bR} Bright Red ${cRs}"
@@ -28,5 +29,5 @@ echo -e "${bM} Bright Magenta${cRs}"
 echo -e "${bP} Bright Purple${cRs}"
 echo -e "${bC} Bright Cyan${cRs}"
 
-echo -e "Symbols"
+echo -e "\n Symbols"
 echo -e "${GCK} Green Check Mark"
