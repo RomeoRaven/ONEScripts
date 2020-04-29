08a1b4cce8b9019f1903af90c6682a9695a6e4f8
Dennis F <romeoraven@gmail.com>
2020-04-29 14:49:08 -0400

    Added Default color and updated RESET variables

--- colors_test.sh
+++ colors_test.sh
@@ -1,26 +1,32 @@
 #!/bin/bsh
 
-## Color Test script
+## Color Test scRsipt
 
-#Pull color code script
-source <(curl -ks https://raw.githubusercontent.com/RomeoRaven/ONEScripts/master/colors.sh);
-# default: echo -e "${cG}THING${cR}"
+#Pull color code scrsipt
+source <(curl -ks https://raw.githubusercontent.com/RomeoRaven/ONEScrsipts/master/colors.sh);
+# default: echo -e "${cG}THING${cRs}"
 
-echo -e "Colors   |  ${fB}Color Bold${fR}  |  ${fU}Color Underline${fR}  |   ${fH}Color Blink${fR}   |    ${fS}Color Shade${fR}   |"
-echo -e "Default  | ${fB} Default Bold${fR} | ${fU}Default Underline${cR}${fR}  | ${fBl} Default Blink${cR}${fR}  | ${fS} Default Shade${cR}${fR} "
-echo -e "${cN} Black   | ${cN}${fB} Black Bold${fR}   | ${cN}${fU}Black Underline${cR}${fR}    | ${cN}${fBl} Black Blink${cR}${fR}    | ${cN}${fS} Black Shade${cR}${fR} "
-echo -e "${cG} Green   | ${cG}${fB} Green Bold${fR}   | ${cG}${fU}Green Underline${cR}${fR}    | ${cG}${fBl} Green Blink${cR}${fR}    | ${cG}${fS} Green Shade${cR}${fR} "
-echo -e "${cY} Yellow  | ${cY}${fB} Yellow Bold${fR}  | ${cY}${fU}Yellow Underline${cR}${fR}   | ${cY}${fBl} Yellow Blink${cR}${fR}   | ${cY}${fS} Yellow Shade${cR}${fR} "
-echo -e "${cB} Blue    | ${cB}${fB} Blue Bold${fR}    | ${cB}${fU}Blue Underline${cR}${fR}     | ${cB}${fBl} Blue Blink${cR}${fR}     | ${cB}${fS} Blue Shade${cR}${fR} "
-echo -e "${cM} Magenta | ${cM}${fB} Magenta Bold${fR} | ${cM}${fU}Magenta Underline${cR}${fR}  | ${cM}${fBl} Magenta Blink${cR}${fR}  | ${cM}${fS} Magenta Shade${cR}${fR} "
-echo -e "${cP} Purple  | ${cP}${fB} Purple Bold${fR}  | ${cP}${fU}Purple Underline${cR}${fR}   | ${cP}${fBl} Purple Blink${cR}${fR}   | ${cP}${fS} Purple Shade${cR}${fR} "
-echo -e "${cC} Cyan    | ${cC}${fB} Cyan Bold${fR}    | ${cC}${fU}Cyan Underline${cR}${fR}     | ${cC}${fBl} Cyan Blink${cR}${fR}     | ${cC}${fS} Cyan Shade${cR}${fR} "
+echo ""
+echo -e "  Colors   |  ${fB}Color Bold${fR}  |  ${fU}Color Underline${fR}  |   ${fH}Color Blink${fR}   |    ${fS}Color Shade${fR}   |"
+echo -e " Default  | ${fB} Default Bold${fR} | ${fU}Default Underline${fR}  | ${fBl} Default Blink${fR}  | ${fS} Default Shade${fR} "
+echo -e "${cN} Black   | ${cN}${fB} Black Bold${fR}   | ${cN}${fU}Black Underline${cRs}${fR}    | ${cN}${fBl} Black Blink${cRs}${fR}    | ${cN}${fS} Black Shade${cRs}${fR} "
+echo -e "${cR} Red     | ${cR}${fB} Red Bold${fR}     | ${cR}${fU}Red Underline${cRs}${fR}      | ${cR}${fBl} Red Blink${cRs}${fR}      | ${cR}${fS} Red Shade${cRs}${fR} "
+echo -e "${cG} Green   | ${cG}${fB} Green Bold${fR}   | ${cG}${fU}Green Underline${cRs}${fR}    | ${cG}${fBl} Green Blink${cRs}${fR}    | ${cG}${fS} Green Shade${cRs}${fR} "
+echo -e "${cY} Yellow  | ${cY}${fB} Yellow Bold${fR}  | ${cY}${fU}Yellow Underline${cRs}${fR}   | ${cY}${fBl} Yellow Blink${cRs}${fR}   | ${cY}${fS} Yellow Shade${cRs}${fR} "
+echo -e "${cB} Blue    | ${cB}${fB} Blue Bold${fR}    | ${cB}${fU}Blue Underline${cRs}${fR}     | ${cB}${fBl} Blue Blink${cRs}${fR}     | ${cB}${fS} Blue Shade${cRs}${fR} "
+echo -e "${cM} Magenta | ${cM}${fB} Magenta Bold${fR} | ${cM}${fU}Magenta Underline${cRs}${fR}  | ${cM}${fBl} Magenta Blink${cRs}${fR}  | ${cM}${fS} Magenta Shade${cRs}${fR} "
+echo -e "${cP} Purple  | ${cP}${fB} Purple Bold${fR}  | ${cP}${fU}Purple Underline${cRs}${fR}   | ${cP}${fBl} Purple Blink${cRs}${fR}   | ${cP}${fS} Purple Shade${cRs}${fR} "
+echo -e "${cC} Cyan    | ${cC}${fB} Cyan Bold${fR}    | ${cC}${fU}Cyan Underline${cRs}${fR}     | ${cC}${fBl} Cyan Blink${cRs}${fR}     | ${cC}${fS} Cyan Shade${cRs}${fR} "
 
-echo -e "Bright = Bold"
-echo -e "${bN} Bright Black ${cR}"
-echo -e "${bG} Bright Green${cR}"
-echo -e "${bY} Bright Yellow${cR}"
-echo -e "${bB} Bright Blue${cR}"
-echo -e "${bM} Bright Magenta${cR}"
-echo -e "${bP} Bright Purple${cR}"
-echo -e "${bC} Bright Cyan${cR}"
+echo -e " Bright = Bold"
+echo -e "${bN} Bright Black ${cRs}"
+echo -e "${bR} Bright Red ${cRs}"
+echo -e "${bG} Bright Green${cRs}"
+echo -e "${bY} Bright Yellow${cRs}"
+echo -e "${bB} Bright Blue${cRs}"
+echo -e "${bM} Bright Magenta${cRs}"
+echo -e "${bP} Bright Purple${cRs}"
+echo -e "${bC} Bright Cyan${cRs}"
+
+echo -e "Symbols"
+echo -e "${GCK} Green Check Mark"
