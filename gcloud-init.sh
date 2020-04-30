ae709f8d4eccfaef9ae6119290396329d048a1f5
Dennis F <romeoraven@gmail.com>
2020-04-30 15:54:30 -0400

    Needed to move a fi lower for Apache secttion

--- gcloud-init.sh
+++ gcloud-init.sh
@@ -9,14 +9,15 @@ echo "Which option do you want?"
 select answer in "More details" "VM Info" "Apache" "Nano" "Index.html" "Index.html -o" "Everything" "Exit"; do
     case $answer in
         "More details" ) curl -ks https://raw.githubusercontent.com/RomeoRaven/ONEScripts/dev/gcloud-init.sh;;
-        "VM Info" )     echo "RUN: bash <(curl -ks https://raw.githubusercontent.com/RomeoRaven/ONEScripts/master/basic-info.sh)"; exit;;
+        "VM Info" )     echo "RUN AS ROOT: bash <(curl -ks https://raw.githubusercontent.com/RomeoRaven/ONEScripts/master/basic-info.sh)"; exit;;
         Apache )        if [ -d /etc/httpd/ ]; then echo "Looks like Apache already installed"; fi;
-                        if [ ! -d /etc/httpd/ ]; then echo "Apache installing"; fi;
-                        dnf install -y httpd;
-                        systemctl enable httpd;
-                        systemctl start httpd;
-                        firewall-cmd --zone=public --permanent --add-service=http;
-                        firewall-cmd --reload;
+                        if [ ! -d /etc/httpd/ ]; then echo "Apache installing";
+                            dnf install -y httpd;
+                            systemctl enable httpd;
+                            systemctl start httpd;
+                            firewall-cmd --zone=public --permanent --add-service=http;
+                            firewall-cmd --reload;
+                        fi;
                         ;;
         Nano )          echo "Yum installing nano";;
         Index.html )    if [ -f /var/www/html/index.html ]; then echo "Index.html already exists, do you want to overwrite it? If so select the Index.html -o option"; fi;
